2db4c812f8f45477c5ce8520c9f3395168d3838c
Vladik <optimus014@gmail.com>
2019-04-30 20:00:24 +0300

    Updates

---
+++ precache-manifest.6ef3b5ff2f9e5044780558f71b08f197.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a620a54bced85bd6ff9a8d5c29fd655",
+    "url": "/live-love-react/index.html"
+  },
+  {
+    "revision": "f321ab41b71f49ec9dd6",
+    "url": "/live-love-react/static/css/main.58f21737.chunk.css"
+  },
+  {
+    "revision": "07cb50f0ffe62b50934e",
+    "url": "/live-love-react/static/js/2.78b44f00.chunk.js"
+  },
+  {
+    "revision": "f321ab41b71f49ec9dd6",
+    "url": "/live-love-react/static/js/main.ae5a7f5d.chunk.js"
+  },
+  {
+    "revision": "c51753efc8d5ff1610c3",
+    "url": "/live-love-react/static/js/runtime~main.8ff0b47a.js"
+  },
+  {
+    "revision": "dbac17065cb6472b92a8d88b2e668429",
+    "url": "/live-love-react/static/media/live-logo.dbac1706.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66932adb5f4355508fad3f9225a78fae.js"
+  "/live-love-react/precache-manifest.6ef3b5ff2f9e5044780558f71b08f197.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/live-love-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
